05f0cc8992784caf889369b7df5714f0d1bdebb5
Nils-Erik Schulz <noni-online@outlook.de>
2019-10-05 16:17:46 +0200

    Handle video files with flawed timestamps

--- stream.sh
+++ stream.sh
@@ -8,14 +8,14 @@ do
     if (( $i > 0 )); then
         tee_muxer_output+="|"
     fi
-    tee_muxer_output+="[f=fifo:fifo_format=flv:queue_size=120:drop_pkts_on_overflow=1:attempt_recovery=1:recovery_wait_time=10:max_recovery_attempts=5:restart_with_keyframe=1]${platform}"
+    tee_muxer_output+="[f=fifo:fifo_format=flv:queue_size=120:drop_pkts_on_overflow=1:attempt_recovery=1:recovery_wait_time=10:max_recovery_attempts=0:restart_with_keyframe=1]${platform}"
     i=$((i+1))
 done
 
 while true :
 do
-	ffmpeg -threads "${THREAD_COUNT}" -loglevel debug \
-		-fflags +igndts+genpts+discardcorrupt \
+	ffmpeg -threads "${THREAD_COUNT}" -loglevel warning \
+		-fflags "+autobsf+genpts+discardcorrupt" -avoid_negative_ts "make_zero" -copytb 1 \
 		-re -stream_loop -1 -i "${BACKGROUND}" \
 		-f alsa -ac 2 -thread_queue_size 1024 -i hw:Loopback,1,0 -c:v copy -c:a aac -filter:a "volume=${VOLUME}" -map 0:v -map 1:a \
 		-f tee "${tee_muxer_output}"
